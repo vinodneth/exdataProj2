4a274358cd1b0e7fc8ea731af12b5a50703e53d2
vinodnet <vinodnet@yahoo.com>
2017-07-08 16:05:16 +0530

    Final version submitted

---
+++ plot4.R
@@ -0,0 +1,26 @@
+## Exploratory Data - Plot 4
+
+# Load the NEI & SCC data frames.
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Subset coal combustion related NEI data
+combustionRelated <- grepl("comb", SCC$SCC.Level.One, ignore.case=TRUE)
+coalRelated <- grepl("coal", SCC$SCC.Level.Four, ignore.case=TRUE) 
+coalCombustion <- (combustionRelated & coalRelated)
+combustionSCC <- SCC[coalCombustion,]$SCC
+combustionNEI <- NEI[NEI$SCC %in% combustionSCC,]
+
+png("plot4.png",width=480,height=480,units="px",bg="transparent")
+
+library(ggplot2)
+
+ggp <- ggplot(combustionNEI,aes(factor(year),Emissions)) +
+  geom_bar(stat="identity",fill="grey",width=0.75) +
+  theme_bw() +  guides(fill=FALSE) +
+  labs(x="year", y="Total PM2.5 Emission (Tons)") + 
+  labs(title="PM2.5 Coal Combustion Source Emissions Across US from 1999-2008")
+
+print(ggp)
+
+dev.off()
---
+++ plot5.R
@@ -0,0 +1,27 @@
+## Exploratory Data - Plot 5
+
+# Load the NEI & SCC data frames.
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Gather the subset of the NEI data which corresponds to vehicles
+vehicles <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
+vehiclesSCC <- SCC[vehicles,]$SCC
+vehiclesNEI <- NEI[NEI$SCC %in% vehiclesSCC,]
+
+# Subset the vehicles NEI data to Baltimore's fip
+baltimoreVehiclesNEI <- vehiclesNEI[vehiclesNEI$fips=="24510",]
+
+png("plot5.png",width=480,height=480,units="px",bg="transparent")
+
+library(ggplot2)
+
+ggp <- ggplot(baltimoreVehiclesNEI,aes(factor(year),Emissions)) +
+  geom_bar(stat="identity",fill="grey",width=0.75) +
+  theme_bw() +  guides(fill=FALSE) +
+  labs(x="year", y="Total PM2.5 Emission (Tons)") + 
+  labs(title="PM2.5 Motor Vehicle Source Emissions in Baltimore from 1999-2008")
+
+print(ggp)
+
+dev.off()
